bd7dad889508ec4c3d0fdde9a019c0180caeddba
josermf2 <62853525+josermf2@users.noreply.github.com>
2021-06-14 15:42:37 -0300

    implementa os testes da classe TrackTest

--- test/br/edu/insper/desagil/backend/model/TrackTest.java
+++ test/br/edu/insper/desagil/backend/model/TrackTest.java
@@ -2,66 +2,92 @@ package br.edu.insper.desagil.backend.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TrackTest {
+class TrackTest {	
 	@BeforeEach
 	void setUp() {
 	}
 
 	@Test
 	void testZeroSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 0);
+		assertEquals("0:00", track.getDurationString());
 	}
 
 	@Test
 	void testFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 5);
+		assertEquals("0:05", track.getDurationString());
 	}
+
 
 	@Test
 	void testTwentyFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 25);
+		assertEquals("0:25", track.getDurationString());
 	}
+
 
 	@Test
 	void testOneMinuteZeroSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 60);
+		assertEquals("1:00", track.getDurationString());
 	}
+
 
 	@Test
 	void testOneMinuteFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 65);
+		assertEquals("1:05", track.getDurationString());
 	}
+
 
 	@Test
 	void testOneMinuteTwentyFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 85);
+		assertEquals("1:25", track.getDurationString());
 	}
+
 
 	@Test
 	void testTwoMinutesZeroSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 120);
+		assertEquals("2:00", track.getDurationString());
 	}
+
 
 	@Test
 	void testTwoMinutesFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 125);
+		assertEquals("2:05", track.getDurationString());
 	}
+
 
 	@Test
 	void testTwoMinutesTwentyFiveSeconds() {
-		assertEquals(true, false);
+		Track track = new Track(new Artist("Jose"), "A+ em DesAgil", 145);
+		assertEquals("2:25", track.getDurationString());
 	}
+
 
 	@Test
 	void testOneCollaborator() {
-		assertEquals(true, false);
+		List<Artist> colaboradores = new ArrayList<>(); 
+		colaboradores.add(new Artist("Becky G"));
+		CollaborationTrack collaborationTrack = new CollaborationTrack(new Artist("Anitta"), colaboradores ,"A+ em DesAgil", 145);
+		assertEquals("Anitta (feat. Becky G)", collaborationTrack.getFullArtistName());
 	}
 
 	@Test
 	void testTwoCollaborators() {
-		assertEquals(true, false);
-	}
+		List<Artist> colaboradores = new ArrayList<>(); 
+		colaboradores.add(new Artist("Ludmilla"));
+		colaboradores.add(new Artist("Snoop Dog"));
+		CollaborationTrack collaborationTrack = new CollaborationTrack(new Artist("Anitta"), colaboradores ,"A+ em DesAgil", 145);
+		assertEquals("Anitta (feat. Ludmilla, Snoop Dog)", collaborationTrack.getFullArtistName());	}
 }
